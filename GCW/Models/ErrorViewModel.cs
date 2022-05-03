6f466ff9fa62087227250c8dac174de288321a55
Biman Lakhey <biman.lakhey74@gmail.com>
2022-05-03 15:32:35 +0545

    initial commit

---
+++ GCW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GCW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
